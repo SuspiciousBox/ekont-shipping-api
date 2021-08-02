d9fd10352c1069b279c8ac09dc70fca629fe42e7
Ivaylo <43470453+SuspiciousBox@users.noreply.github.com>
2021-08-02 12:34:01 +0300

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cities from "./routes/cities";
+import offices from "./routes/offices";
+import streets from "./routes/streets";
+import lables from "./routes/lables";
+
+const app = express();
+
+//Authentication Header for the API call
+app.use((_req, res, next) => {
+    res.setHeader('Authentication', 'Basic aWFzcC1kZXY6aWFzcC1kZXY=')
+    next()
+})
+
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.post("/cities", cities);
+app.post("/offices", offices);
+app.post("/streets", streets);
+app.post("/lables", lables);
+
+
+//Server configuration
+app.listen(3000, () => {
+    console.log("Server Started on http://localhost:3000")
+});
