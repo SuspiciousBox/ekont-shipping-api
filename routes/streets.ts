afcda061475c7b67bdb159720f90e1af53c6c0c0
Ivaylo <43470453+SuspiciousBox@users.noreply.github.com>
2021-08-02 18:06:06 +0300

    Update streets.ts

--- routes/streets.ts
+++ routes/streets.ts
@@ -1,24 +1,20 @@
-import type {Request, Response} from "express";
+import type { Request, Response } from "express";
 import axios from "axios";
 
-export default async (req : Request, res : Response) => {
-    axios
-    .post('https://ee.econt.com/services/Nomenclatures/NomenclaturesService.getStreets.json', {
-        cityID: req.body.cityID,
-      })
-      .then(response => res.json(JSON.stringify(response.data)))
-      .catch(async (error) => {
-        if (error.response) {
-          // Request made and server responded
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        } else if (error.request) {
-          // The request was made but no response was received
-          console.log(error.request);
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log('Error', error.message);
-        }
-    });
-}
+export default async (req: Request, res: Response) => {
+  const streetsRes = await axios.post(
+    "https://ee.econt.com/services/Nomenclatures/NomenclaturesService.getStreets.json",
+    {
+      countryCode: "BGR",
+    }
+  );
+  const streets: Record<string, any>[] = streetsRes.data.streets;
+
+  const result = streets.map(({ id, cityID, name, nameEn }) => ({
+    id,
+    cityID,
+    name,
+    nameEn,
+  }));
+  res.json(result);
+};
