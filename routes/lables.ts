477e8e8d2a56bd9d8292a5c3ff3c6fcde700ac13
Ivaylo <43470453+SuspiciousBox@users.noreply.github.com>
2021-08-02 13:26:59 +0300

    Add files via upload

---
+++ routes/lables.ts
@@ -0,0 +1,26 @@
+import type {Request, Response} from "express";
+import axios from "axios";
+
+export default async (req : Request, res : Response) => {
+    axios
+    .post('https://ee.econt.com/services/Shipments/LabelService.createLabel.json', {
+        countryCode: 'BGR',
+        label: req.body.label ,
+        mode: 'calculate'
+      })
+      .then(response => res.json(response.data))
+      .catch(async (error) => {
+        if (error.response) {
+          // Request made and server responded
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+    });
+}
---
+++ routes/offices.ts
@@ -0,0 +1,25 @@
+import type {Request, Response} from "express";
+import axios from "axios";
+
+export default async (req : Request, res : Response) => {
+    axios
+    .post('https://ee.econt.com/services/Nomenclatures/NomenclaturesService.getOffices.json', {
+        countryCode: 'BGR',
+        cityID: req.body.cityID
+      })
+      .then(response => res.json(response.data))
+      .catch(async (error) => {
+        if (error.response) {
+          // Request made and server responded
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+    });
+}
