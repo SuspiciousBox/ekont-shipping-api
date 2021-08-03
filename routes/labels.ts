a810f01537b86393b7520bd0dcb9fd7a17ac9494
Ivaylo <43470453+SuspiciousBox@users.noreply.github.com>
2021-08-03 17:01:38 +0300

    Update labels.ts

--- routes/labels.ts
+++ routes/labels.ts
@@ -2,32 +2,45 @@ import { Request, Response } from "express";
 import axios from "axios";
 
 export default async (req: Request, res: Response) => {
-  const labelRes = await axios.post(
-    "https://ee.econt.com/services/Shipments/LabelService.createLabel.json",
-    {
-      label: req.body.label,
-      senderClient: req.body.senderClient,
-      senderAddress: req.body.senderAddress,
-      receiverClient: req.body.receiverClient,
-      receiverAddress: req.body.receiverAddress,
-      shipmentType: "PACK",
-      weight: req.body.weight,
-      mode: "calculate",
-    }
-  );
-  const result: Record<string, any>[] = labelRes.data.label;
-
-  /*
-  const result = label.map(({ label }) => ({
-  label,
-  }));
-  */
   try {
-    res.json(result);
-  } catch (err) {
-    console.log(err.response.data);
-    console.log(err.response.status);
-    console.log(err.response.headers);
-    console.log(err.request);
+    const labelRes = await axios.post(
+      "https://ee.econt.com/services/Shipments/LabelService.createLabel.json",
+      {
+        label: req.body.label,
+        senderClient: req.body.senderClient,
+        senderOfficeCode: req.body.senderOfficeCode,
+        senderAddress: req.body.senderAddress,
+        receiverOfficeCode: req.body.receiverOfficeCode,
+        receiverClient: req.body.receiverClient,
+        receiverAddress: req.body.receiverAddress,
+        shipmentType: "PACK",
+        weight: req.body.weight,
+        mode: "calculate",
+      }
+    );
+
+    const result: Record<string, any>[] = labelRes.data.label;
+
+    res.json(result).end;
+  } catch (error) {
+    // Error
+    if (error.response) {
+      /*
+       * The request was made and the server responded with a
+       * status code that falls out of the range of 2xx
+       */
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      /*
+       * The request was made but no response was received
+       */
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request and triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error);
   }
 };
