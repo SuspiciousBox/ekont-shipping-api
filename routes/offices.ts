96764597581fea2c784dfd76ad9f5f5adeed187f
Ivaylo <43470453+SuspiciousBox@users.noreply.github.com>
2021-08-03 17:06:29 +0300

    Update offices.ts

--- routes/offices.ts
+++ routes/offices.ts
@@ -1,29 +1,44 @@
-
 import { Request, Response } from "express";
 import axios from "axios";
 
 export default async (req: Request, res: Response) => {
-  const officesRes = await axios.post(
-    "https://ee.econt.com/services/Nomenclatures/NomenclaturesService.getOffices.json",
-    {
-      countryCode: "BGR",
-      cityID: req.body.cityID,
-    }
-  );
-  const offices: Record<string, any>[] = officesRes.data.offices;
-
-  const result = offices.map(({ id, code, name, nameEn }) => ({
-    id,
-    code,
-    name,
-    nameEn,
-  }));
   try {
-    res.json(result);
-  } catch (err) {
-    console.log(err.response.data);
-    console.log(err.response.status);
-    console.log(err.response.headers);
-    console.log(err.request);
+    const officesRes = await axios.post(
+      "https://ee.econt.com/services/Nomenclatures/NomenclaturesService.getOffices.json",
+      {
+        countryCode: "BGR",
+        cityID: req.body.cityID,
+      }
+    );
+    const offices: Record<string, any>[] = officesRes.data.offices;
+
+    const result = offices.map(({ id, code, name, nameEn }) => ({
+      id,
+      code,
+      name,
+      nameEn,
+    }));
+
+    res.json(result).end;
+  } catch (error) {
+    // Error
+    if (error.response) {
+      /*
+       * The request was made and the server responded with a
+       * status code that falls out of the range of 2xx
+       */
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      /*
+       * The request was made but no response was received
+       */
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request and triggered an Error
+      console.log("Error", error.message);
+    }
+    console.log(error);
   }
 };
